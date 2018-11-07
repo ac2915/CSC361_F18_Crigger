4cde48b13ea9005d2ef0c14a7899d70e5e24ddfa
Allen Crigger <ac2915@cs.ship.edu>
2018-11-07 14:24:00 -0500

    TextureAtlas Created.

--- desktop/src/com/cherryscramble/game/desktop/DesktopLauncher.java
+++ desktop/src/com/cherryscramble/game/desktop/DesktopLauncher.java
@@ -1,27 +1,47 @@
+/**
+ * Class contains the properties for CherryScramble's desktop launcher.
+ */
+
 package com.cherryscramble.game.desktop;
 
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.cherryscramble.g1.CherryScrambleMain;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	
+	private static boolean rebuildAtlas = false;		// Boolean of atlas rebuild
+	private static boolean drawDebugOutline = false;	// Boolean for draw debug
+	
+	public static void main (String[] arg) {	
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration(); //Configuration
 		
 		//
 		// Window variables for desktop
 		//
 		
 		// -= Window Title and Icon =-
-		config.title = "Cherry Scramble!";  // Window Title
-		config.addIcon("assets-raw/icons/CherryIcon.png", Files.FileType.Internal);     //Window Icon
+		config.title = "Cherry Scramble!";  										// Window Title
+		config.addIcon("assets-raw/icons/CherryIcon.png", Files.FileType.Internal); // Window Icon
 		
 		// -= Window Resolution =-
-		config.height = 576; // Window Height (18 tiles high)
-		config.width = 800;  // Window Width  (32 tiles wide)
+		config.height = 576; 		// Window Height (18 tiles high)
+		config.width = 800;  		// Window Width  (32 tiles wide)
+		config.resizable = false; 	// Prevents Resizing of the window
 		
-		
+		// Rebuilds the texture atlases when they need to be recompiled
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.debug = drawDebugOutline; //Draws an outline around the images
+			
+			// Process the textures and merge it into a texture atlas
+			TexturePacker.process(settings, "assets-raw/images", "../core/assets/images", "cherryscramble.atlas");
+		}
 		new LwjglApplication(new CherryScrambleMain(), config); //Create application
 	}
 }
